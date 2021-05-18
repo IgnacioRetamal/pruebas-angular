482214d85fcf9ff3264de7cd06f983292e0e2379
IgnacioRetamal <ignacioretamal01@gmail.com>
2021-05-17 23:29:13 -0300

    tp-03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{ ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HijoComponent } from './componente-hijo/hijo.component';
+import { PadreComponent } from './componente-padre/padre.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent, PadreComponent, HijoComponent ],
+  imports: [ BrowserModule, AppRoutingModule, BrowserAnimationsModule, ReactiveFormsModule, MatButtonModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
